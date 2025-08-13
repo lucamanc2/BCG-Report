deb0f86fafa0175783d45355dae2f6ff6ebd6ca9
lucamanc2 <luke_999@hotmail.it>
2025-08-13 14:07:31 +0200

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,8 @@
-"use client";
 import BCGWebApp from "@/components/BCGWebApp";
 
 export default function Page() {
   return (
-    <main>
+    <main className="min-h-screen p-6">
       <BCGWebApp />
     </main>
   );
